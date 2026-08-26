e34b2fa231599d06d4b08a7a1aef0ae35ba921e4
Chernykh Artyom <chernykhav@bcs.ru>
2026-08-27 01:26:47 +0300

    random storage box product positions

--- Assets/Scripts/MarketParty/Interactables/StorageStand.cs
+++ Assets/Scripts/MarketParty/Interactables/StorageStand.cs
@@ -22,9 +22,11 @@ namespace MarketParty.Interactables
 
             var product = Instantiate(ProductsManager.Instance.GetRandomProductPrefab(_productTag), transform.position, Quaternion.identity);
 
+            var randomStorageBoxPoint = new Vector3(Random.Range(-0.19f, 0.19f), 0f, Random.Range(-0.15f, 0.15f));
+
             // todo add rigidbody, remove parent.
             product.transform.SetParent(storageBox.transform);
-            product.transform.DOLocalJump(Vector3.zero, 0.5f, 1, 0.5f);
+            product.transform.DOLocalJump(randomStorageBoxPoint, 0.5f, 1, 0.5f);
 
             storageBox.AddProduct(product);
 
